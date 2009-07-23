2131ac70fe7e75df1fffaacdf72d5044a9cfcd20
alberto.bietti <alberto.bietti@555e82ec-7758-11de-a2af-c98509b8391b>
2009-07-23 08:34:45 +0000

    segfault: OpenCV ERROR: Sizes of input arguments do not match (Not all the planes have equal steps) in function cvCalcArrBackProject

--- trunk/colorIndexing/main.cpp
+++ trunk/colorIndexing/main.cpp
@@ -6,13 +6,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  
-  char *image1, *image2;
-  
-  if (argc < 3 || argc > 3) {
-  	cerr << "Usage: " << argv[0] << " template target" << endl;
-  	return 0;
-  }
+  CvCapture* capture = cvCreateCameraCapture(0);
 
   IplImage* refImg = cvLoadImage(argv[1]);
   int arr[2] = {16, 16};
@@ -39,44 +33,55 @@ int main(int argc, char *argv[])
   IplImage* imgArr[2] = { refImgH, refImgS };
   cvCalcHist( imgArr, refHist);
 
+  cvNamedWindow("webcam", CV_WINDOW_AUTOSIZE);
   cvNamedWindow("hist", CV_WINDOW_AUTOSIZE);
   //cvShowImage("hist", refHist->bins);
 
-  IplImage* testImg = cvLoadImage(argv[2]);
-  CvHistogram* testHist = cvCreateHist(2, arr, CV_HIST_ARRAY);
+  while (true)
+    {
+      IplImage* testImg = cvQueryFrame(capture);
+      cvShowImage("webcam", testImg);
+      CvHistogram* testHist = cvCreateHist(2, arr, CV_HIST_ARRAY);
 
-  IplImage *testArr = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 3);
-  IplImage *testImgH = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 1);
-  IplImage *testImgS = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 1);
-  IplImage *testImgV = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 1);
+      IplImage *testArr = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 3);
+      IplImage *testImgH = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 1);
+      IplImage *testImgS = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 1);
+      IplImage *testImgV = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_8U, 1);
 
-  //cvCvtColor(testImg, refArr, CV_RGBA2RGB);
-  cvCvtColor(testImg, testArr, CV_RGB2HSV);
-  cvSplit(testArr, testImgH, testImgS, testImgV, NULL);
+      //cvCvtColor(testImg, refArr, CV_RGBA2RGB);
+      cvCvtColor(testImg, testArr, CV_RGB2HSV);
+      cvSplit(testArr, testImgH, testImgS, testImgV, NULL);
 
-  imgArr[0] = testImgH;
-  imgArr[1] =  testImgS;
-  cvCalcHist( imgArr, testHist);
+      imgArr[0] = testImgH;
+      imgArr[1] =  testImgS;
+      cvCalcHist( imgArr, testHist);
 
-  double comp = cvCompareHist(testHist, refHist, CV_COMP_INTERSECT);
+      double comp = cvCompareHist(testHist, refHist, CV_COMP_INTERSECT);
 
-  cout << comp << endl;
+      cout << comp << endl;
   
-  IplImage* patchImg = cvCreateImage(cvSize(testImg->width - 19, testImg->height - 19), IPL_DEPTH_32F, 1);
-  cvCalcBackProjectPatch(imgArr, patchImg, cvSize(20, 20), refHist, CV_COMP_INTERSECT, 5);
+      IplImage* patchImg = cvCreateImage(cvGetSize(testImg), IPL_DEPTH_32F, 1);
+      //cvCalcBackProjectPatch(imgArr, patchImg, cvSize(1, 1), refHist, CV_COMP_INTERSECT, 5);
+      cvCalcBackProject(imgArr, patchImg, refHist);
 
-  cvShowImage("hist", patchImg);
+      cvShowImage("hist", patchImg);
 
-  CvPoint max, min;
-  cvMinMaxLoc(patchImg, NULL, NULL, &min, &max);
+      CvPoint max, min;
+      cvMinMaxLoc(patchImg, NULL, NULL, &min, &max);
 
-  cout << min.x << " " << min.y << endl;
-  cout << max.x << " " << max.y << endl;
+      cout << "min: " << min.x << " " << min.y << endl;
+      cout << "max: " << max.x << " " << max.y << endl;
+
+      if (cvWaitKey(33) != -1)
+	break;
+    }
 
   cvWaitKey(0);
+  cvReleaseCapture(&capture);
   cvDestroyWindow("ImgH");
   cvDestroyWindow("ImgS");
   cvDestroyWindow("ImgV");
   cvDestroyWindow("hist");
+  cvDestroyWindow("webcam");
   return 0;
 }
